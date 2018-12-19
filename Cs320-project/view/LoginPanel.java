8218b3e5ffc8ad6d1c940d9ca88db71fde7a559d
ataberkozek <43850810+ataberkozek@users.noreply.github.com>
2018-12-19 18:06:06 +0300

    created login gui and its listeners.

---
+++ Cs320-project/view/LoginPanel.java
@@ -0,0 +1,60 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Arrays;
+
+
+public class LoginPanel extends JFrame {
+    String username = "username";
+    String password = "pass";
+    JTextField txtUsername;
+    JPasswordField txtPassword;
+
+
+    public static void main(String args[]) {
+        LoginPanel login = new LoginPanel();
+        login.initialize();
+    }
+
+    private void initialize() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel panel = new JPanel();
+        JLabel UsernameLabel = new JLabel("Username:");
+        JLabel PasswordLabel = new JLabel("Password:");
+        txtUsername = new JTextField(20);
+        txtPassword = new JPasswordField(20);
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new LoginListener());
+        JButton cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(new CancelListener());
+
+        panel.add(UsernameLabel);
+        panel.add(txtUsername);
+        panel.add(PasswordLabel);
+        panel.add(txtPassword);
+        panel.add(loginButton);
+        panel.add(cancelButton);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setSize(250,200);
+        frame.setVisible(true);
+
+    }
+
+    public class LoginListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            if (username.equals(txtUsername.getText()) && Arrays.equals(password.toCharArray(), txtPassword.getPassword())) {
+                JOptionPane.showMessageDialog(null, "Logged in successfully!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Log in failed!");
+            }
+        }
+    }
+
+    public class CancelListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+}
