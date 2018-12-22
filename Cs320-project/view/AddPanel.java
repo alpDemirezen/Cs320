b3ba53e4de4445f4a06c999453282d546e3f80ff
ahmetgokce <43876641+ahmetgokce@users.noreply.github.com>
2018-12-23 02:05:15 +0300

    TypePanel and AddPanelHouse are added.

--- Cs320-project/view/AddPanel.java
+++ Cs320-project/view/AddPanel.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class AddPanel extends JPanel {
+public class AddPanel extends JFrame {
 
     void initialize(){
 
